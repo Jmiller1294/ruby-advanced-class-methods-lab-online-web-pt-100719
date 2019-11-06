a31522682bded31bdf7794d45b5e40f01d46ff88
Justin Miller <jayman1294@gmail.com>
2019-11-06 06:38:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,10 +35,10 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-      if self.all.find_by_name(name) == nil
+      if self.find_by_name(name) == nil
         self.create_by_name
       else 
-        self.all.find_by_name(name)
+        self.find_by_name(name)
       end 
   end
 end
